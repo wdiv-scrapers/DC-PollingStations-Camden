7c37e666f0ce142fd6e97a2cad482f93dd2eb245
chris48s <c.mshaw@yahoo.co.uk>
2016-11-19 17:12:04 +0000

    Use KML outputs for districts
    I think the json output uses inconsistent ids.
    Lets see if the KML is more stable...

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
 stations_url = "https://opendata.camden.gov.uk/api/views/5rhh-fxna/rows.xml?accessType=DOWNLOAD"
-districts_url = "https://opendata.camden.gov.uk/api/views/ta65-2szc/rows.json?accessType=DOWNLOAD"
+districts_url = "https://opendata.camden.gov.uk/api/geospatial/p2bh-u5fm?method=export&format=KML"
 council_id = 'E09000007'
 
 
