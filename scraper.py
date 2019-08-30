95e7efe96df2d98b4c39c96c1d3fe7e94ae07f35
chris48s <chris.shaw480@gmail.com>
2019-08-30 11:47:53 +0100

    switch from XML to CSV format

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
-stations_url = "https://opendata.camden.gov.uk/api/views/5rhh-fxna/rows.xml?accessType=DOWNLOAD"
+stations_url = "https://opendata.camden.gov.uk/api/views/5rhh-fxna/rows.csv?accessType=DOWNLOAD"
 districts_url = "https://opendata.camden.gov.uk/api/geospatial/p2bh-u5fm?method=export&format=KML"
 council_id = 'E09000007'
 
